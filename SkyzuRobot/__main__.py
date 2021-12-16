00994b540b94d9b621324654e3c74e96cc011b63
gabuttteet <95076888+reygabuttt@users.noreply.github.com>
2021-12-16 19:15:23 +0700

    Update __main__.py

--- SkyzuRobot/__main__.py
+++ SkyzuRobot/__main__.py
@@ -83,8 +83,8 @@ def get_readable_time(seconds: int) -> str:
 PM_START_TEXT = """
 *Hello {} !*
 ────────────────────────
-🔥 I'm an anime theme bot designed to help manage your telegram group with a lot features.
-🔥 Maintained by [Kitaro](https://t.me/ Kitarohey)
+🔥 UwU,I'm an anime theme bot designed to help manage your telegram group with a lot features.
+🔥 Maintained by [ReYZ](https://t.me/ryinaja)
 ────────────────────────
 Hit the /help to see available command.
 """
@@ -101,7 +101,7 @@ buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​", url="https://github.com/Kitaroo/KitaroRobot"
+            text="sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ​", url="https://github.com/memek"
         ),
         InlineKeyboardButton(
             text="sᴜᴘᴘᴏʀᴛ​", url=f"https://t.me/{SUPPORT_CHAT}"
@@ -109,17 +109,17 @@ buttons = [
     ],
     [
         InlineKeyboardButton(
-            text="➗ ᴀᴅᴅ ᴋɪᴛᴀʀᴏ ʀᴏʙᴏᴛ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➗", url="t.me/Kitaromusic_bot?startgroup=new"),
+            text="➗ ᴀᴅᴅ ᴇʟᴀɪɴᴀ ᴛᴏ ʏᴏᴜʀ ɢʀᴜʙ ➗", url="t.me/ElainaZRobot?startgroup=new"),
     ],
 ]
 
 HELP_STRINGS = """
 Click on the button bellow to get description about specifics command."""
 
-EMI_IMG = "https://telegra.ph/file/d53c76029ab708bdfc7f6.jpg"
+EMI_IMG = "https://telegra.ph/file/032738e2726c08d04859a.jpg"
 
 DONATE_STRING = """Hey, glad to hear you want to donate!
- You can support the project by contacting @Kitarohey \
+ You can support the project by contacting @ryinaja \
  Supporting isnt always financial! \
  Those who cannot provide monetary support are welcome to help us develop the bot at ."""
 
@@ -235,7 +235,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            f"<b>Hi I'm Kitaro robot!</b>\n<b>Started working since:</b> <code>{uptime}</code>",
+            f"<b>Hi I'm Elaina robot!</b>\n<b>Started working since:</b> <code>{uptime}</code>",
             parse_mode=ParseMode.HTML
        )
 
@@ -366,15 +366,15 @@ def Skyzu_about_callback(update, context):
     query = update.callback_query
     if query.data == "Skyzu_":
         query.message.edit_text(
-            text="๏ I'm *KITARO*, a powerful group management bot built to help you manage your group easily."
+            text="๏ I'm *ELAINA*, a powerful group management bot built to help you manage your group easily."
             "\n• I can restrict users."
             "\n• I can greet users with customizable welcome messages and even set a group's rules."
             "\n• I have an advanced anti-flood system."
             "\n• I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc."
             "\n• I have a note keeping system, blacklists, and even predetermined replies on certain keywords."
             "\n• I check for admins' permissions before executing any command and more stuffs"
-            "\n\n_Kitaro's licensed under the GNU General Public License v3.0_"
-            "\n\n Click on button bellow to get basic help for KitaroRobot.",
+            "\n\n_Elaina's licensed under the GNU General Public License v3.0_"
+            "\n\n Click on button bellow to get basic help for ElainaRobot.",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
@@ -414,7 +414,7 @@ def Skyzu_about_callback(update, context):
     elif query.data == "Skyzu_admin":
         query.message.edit_text(
             text=f"*๏ Let's make your group bit effective now*"
-            "\nCongragulations, KitaroRobot now ready to manage your group."
+            "\nCongragulations, ElainaRobot now ready to manage your group."
             "\n\n*Admin Tools*"
             "\nBasic Admin tools help you to protect and powerup your group."
             "\nYou can ban members, Kick members, Promote someone as admin through commands of bot."
@@ -441,13 +441,13 @@ def Skyzu_about_callback(update, context):
         )
     elif query.data == "Skyzu_support":
         query.message.edit_text(
-            text="*๏ Kitaro support chats*"
-            "\nJoin My Support Group/Channel for see or report a problem on Skyzu.",
+            text="*๏ Elaina support chats*"
+            "\nJoin My Support Group/Channel for see or report a problem on Elaina.",
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
                 [
                  [
-                    InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ​", url="t.me/rumahkitaro"),
+                    InlineKeyboardButton(text="sᴜᴘᴘᴏʀᴛ​", url="t.me/ElainaSupport"),
                  ],
                  [
                     InlineKeyboardButton(text="ɢᴏ ʙᴀᴄᴋ​", callback_data="Skyzu_"),
@@ -460,14 +460,14 @@ def Skyzu_about_callback(update, context):
 
     elif query.data == "Skyzu_credit":
         query.message.edit_text(
-            text=f"<b>๏ Credis for KITARO ROBOT</b>\n"
-            f"\nHere Developers Making The KITARO ROBOT",
+            text=f"<b>๏ Credis for ELAINA ROBOT</b>\n"
+            f"\nHere Developers Making The ELAINA ROBOT",
             parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
                 [
                  [
-                    InlineKeyboardButton(text="Kitaro", url="t.me/kitarohey"),
-                    InlineKeyboardButton(text="ZXCSKYY", url="t.me/zxcskyy"),
+                    InlineKeyboardButton(text="ReYZ", url="t.me/ryinaja"),
+                    InlineKeyboardButton(text="Arya", url="t.me/Badboyanim"),
                  ],
                  [
                     InlineKeyboardButton(text="ɢᴏ ʙᴀᴄᴋ​", callback_data="Skyzu_"),
